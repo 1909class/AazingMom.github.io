983bc8962b04ceef7dc1f7ab0a57b0a718bd402a
AazingMom <150440328+AazingMom@users.noreply.github.com>
2024-05-19 18:53:23 +0800

    Delete env.d.ts

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import { ComponentOptions } from 'vue'
-  const componentOptions: ComponentOptions
-  export default componentOptions
-}
-
-declare module 'quill-image-uploader'
